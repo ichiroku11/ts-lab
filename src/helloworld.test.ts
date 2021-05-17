d19299dd7adf67b7ec118c0e9280bea011a18a41
ichiroku11 <toolbox0x10@gmail.com>
2021-05-17 22:53:58 +0900

    Try Deno.test

---
+++ src/helloworld.test.ts
@@ -0,0 +1,10 @@
+// https://deno.land/manual@main/testing
+// https://deno.land/manual@main/testing/assertions
+
+import {
+	assert
+ } from "https://deno.land/std/testing/asserts.ts";
+
+ Deno.test("Hello Test", () => {
+	assert("Hello");
+  });
