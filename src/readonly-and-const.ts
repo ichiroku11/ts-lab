ec30311a1f96cd7df2439bce7951b2c90ae2021b
ichiroku11 <toolbox0x10@gmail.com>
2021-05-27 21:39:07 +0900

    Refactor

--- src/readonly-and-const.ts
+++ src/readonly-and-const.ts
@@ -1,37 +1,38 @@
 // https://www.typescriptlang.org/docs/handbook/typescript-in-5-minutes-func.html#readonly-and-const
 // https://typescript-jp.gitbook.io/deep-dive/type-system/readonly
 
-{
-	// constは変数の値を変更できないようにするだけ
-	// なので配列は値の追加・変更・削除ができる
-	// values1: number[]
-	const values1 = [1, 2, 3];
+// exportするとtsファイル間で名前が衝突しても問題ない様子
+export const readonlyAndConst = null;
 
-	// 追加
-	values1.push(4);
-	console.log(values1);
-	// [1, 2, 3, 4]
+// constは変数の値を変更できないようにするだけ
+// なので配列は値の追加・変更・削除ができる
+// values1: number[]
+const values1 = [1, 2, 3];
 
-	// 変更
-	values1[3] = 0;
-	console.log(values1);
-	// [1, 2, 3, 0]
+// 追加
+values1.push(4);
+console.log(values1);
+// [1, 2, 3, 4]
 
-	// 削除
-	values1.pop();
-	console.log(values1);
-	// [1, 2, 3]
+// 変更
+values1[3] = 0;
+console.log(values1);
+// [1, 2, 3, 0]
 
-	// as constを使うと変数はreadonlyなリテラルになる
-	// values2: readonly [1, 2, 3]
-	const values2 = [1, 2, 3] as const;
-	console.log(values2);
-	// [1, 2, 3]
+// 削除
+values1.pop();
+console.log(values1);
+// [1, 2, 3]
 
-	// 以下はエラー
-	/*
-	values2.push(0);
-	values2[0] = 0;
-	values2.pop();
-	*/
-}
+// as constを使うと変数はreadonlyなリテラルになる
+// values2: readonly [1, 2, 3]
+const values2 = [1, 2, 3] as const;
+console.log(values2);
+// [1, 2, 3]
+
+// 以下はエラー
+/*
+values2.push(0);
+values2[0] = 0;
+values2.pop();
+*/
