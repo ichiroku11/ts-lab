b61b5b575c82f620fc717059513746ad5a7cfc4d
ichiroku11 <toolbox0x10@gmail.com>
2021-05-27 22:12:32 +0900

    Try Partial<Type>

---
+++ src/utility-types-partial.ts
@@ -0,0 +1,30 @@
+// Partial<Type>
+// https://www.typescriptlang.org/docs/handbook/utility-types.html#partialtype
+
+export const utilityTypesPartial = null;
+
+// Partial<Type>でTypeのプロパティがoptionalになる
+
+interface Todo {
+	title: string;
+	description: string;
+}
+
+// dstをfromで更新するメソッド
+function updateTodo(dst: Todo, from: Partial<Todo>): void {
+	if (from.title) {
+		dst.title = from.title;
+	}
+	if (from.description) {
+		dst.description = from.description
+	}
+}
+
+const todo = {
+	title: "abc",
+	description: "def"
+};
+console.log(todo);
+
+updateTodo(todo, { title : "xyz" });
+console.log(todo);
