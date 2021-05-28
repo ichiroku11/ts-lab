347ad69273a200cff0e46564fc17fb3df96ab718
ichiroku11 <toolbox0x10@gmail.com>
2021-05-28 23:31:05 +0900

    Try Required<Type>

---
+++ src/utility-types-required.ts
@@ -0,0 +1,16 @@
+// Required<Type>
+// https://www.typescriptlang.org/docs/handbook/utility-types.html#requiredtype
+
+export const utilityTypesRequired = null;
+
+// Required<Type>でTypeのプロパティが必須になる
+
+interface Sample {
+	value?: number;
+}
+
+const s1: Sample = {};
+console.log(s1);
+
+const s2: Required<Sample> = { value: 0 };
+console.log(s2);
