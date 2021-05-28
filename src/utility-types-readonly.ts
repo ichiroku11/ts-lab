ae2cea848d833994c24eddcf261a55203bc18bb7
ichiroku11 <toolbox0x10@gmail.com>
2021-05-29 08:14:38 +0900

    Try Readonly<Type>

--- src/utility-types-readonly.ts
+++ src/utility-types-readonly.ts
@@ -2,3 +2,17 @@
 // https://www.typescriptlang.org/docs/handbook/utility-types.html#readonlytype
 
 export const nothing = null;
+
+// Readonly<Type>でTypeのプロパティが読み取り専用になる
+
+interface Todo {
+	title: string;
+}
+
+const todo: Readonly<Todo> = {
+	title: "abc"
+};
+console.log(todo);
+
+// Cannot assign to 'title' because it is a read-only property.
+//todo.title = "def";
