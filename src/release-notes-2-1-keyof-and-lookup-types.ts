15f44acaf97e4fe3e10800e418e56dffcd44ce6d
ichiroku11 <toolbox0x10@gmail.com>
2021-05-30 09:44:13 +0900

    Add release-notes-2-1-keyof-and-lookup-types

---
+++ src/release-notes-2-1-keyof-and-lookup-types.ts
@@ -0,0 +1,32 @@
+// keyof and Lookup Types
+// https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-1.html#keyof-and-lookup-types
+// https://www.typescriptlang.org/docs/handbook/2/keyof-types.html#the-keyof-type-operator
+
+
+export const nothing = null;
+
+interface Person {
+	name: string;
+	age: number;
+	location: string;
+}
+
+// keyof T演算子を使うと、Tのプロパティ名の型を取得できる
+
+// PersonKeys型には以下だけを指定できる
+// "name" | "age" | "location"
+type PersonKeys = keyof Person;
+const value: PersonKeys = "age";
+console.log(value);
+
+
+// Lookup types
+// プロパティの値の型を取得できる？
+// PersonNameは、string
+// PersonAgeは、number
+type PersonName = Person["name"];
+type PersonAge = Person["age"];
+const name: PersonName = "abc";
+const age: PersonAge = 1;
+console.log(name);
+console.log(age);
