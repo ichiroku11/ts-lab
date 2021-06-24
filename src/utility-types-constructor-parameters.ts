3b6faa1f65a791f06799b9ba066879b3d38bfdc1
ichiroku11 <toolbox0x10@gmail.com>
2021-06-24 21:26:44 +0900

    Update utility-types-constructor-parameters.ts

--- src/utility-types-constructor-parameters.ts
+++ src/utility-types-constructor-parameters.ts
@@ -3,8 +3,8 @@
 
 export const nothing = null;
 
-// Parameters<Type>
-// Typeで指定した関数の型のパラメータをタプルで表す
+// ConstructorParameters<Type>
+// Typeで指定したコンストラクタの型のパラメータをタプルで表す
 
 // type Type1 = [message?: string | undefined ]
 type Type1 = ConstructorParameters<ErrorConstructor>;
