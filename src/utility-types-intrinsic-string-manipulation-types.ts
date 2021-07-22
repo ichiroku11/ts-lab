1501c07c55f5dc66340bcb391e1732e62b49559b
ichiroku11 <toolbox0x10@gmail.com>
2021-07-22 16:58:34 +0900

    Try intrinsic string manipulation types

---
+++ src/utility-types-intrinsic-string-manipulation-types.ts
@@ -0,0 +1,6 @@
+// Uppercase<StringType>
+// Lowercase<StringType>
+// Capitalize<StringType>
+// Uncapitalize<StringType>
+// https://www.typescriptlang.org/docs/handbook/2/template-literal-types.html#intrinsic-string-manipulation-types
+
