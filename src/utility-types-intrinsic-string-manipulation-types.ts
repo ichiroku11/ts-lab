646e8cb762e0c4729a0782d0e368de70843455dc
ichiroku11 <toolbox0x10@gmail.com>
2021-07-22 17:02:00 +0900

    Try Uppercase<StringType>

--- src/utility-types-intrinsic-string-manipulation-types.ts
+++ src/utility-types-intrinsic-string-manipulation-types.ts
@@ -4,3 +4,19 @@
 // Uncapitalize<StringType>
 // https://www.typescriptlang.org/docs/handbook/2/template-literal-types.html#intrinsic-string-manipulation-types
 
+
+// Uppercase<StringType>
+// StringTypeをすべて大文字に変換する
+type T1 = "hello";
+// type T1Uppercase = "HELLO"
+type T1Uppercase = Uppercase<T1>;
+
+// Lowercase<StringType>
+// todo:
+
+// Capitalize<StringType>
+// todo:
+
+
+// Uncapitalize<StringType>
+// todo:
