5efe621150647a933cd72a8da567206e6860df11
ichiroku11 <toolbox0x10@gmail.com>
2021-07-26 08:12:28 +0900

    Try OmitThisParameter<Type>

--- src/utility-types-omit-this-parameter-type.ts
+++ src/utility-types-omit-this-parameter-type.ts
@@ -1,2 +1,16 @@
 // OmitThisParameter<Type>
 // https://www.typescriptlang.org/docs/handbook/utility-types.html#omitthisparametertype
+// thisパラメータを除いた関数の型を取得する
+
+export const nothing = null;
+
+function toHexString(this: number): string {
+	return this.toString(16);
+}
+
+// const fiveToHexString: () => string
+const fiveToHexString: OmitThisParameter<typeof toHexString> = toHexString.bind(5);
+
+const value = fiveToHexString();
+console.log(value);
+// 5
