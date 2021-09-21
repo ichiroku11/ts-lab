f0493da5310dd36e062f2fe11c62ef14c7aaada6
ichiroku11 <toolbox0x10@gmail.com>
2021-09-22 08:10:41 +0900

    Update indexed-access-types.ts

--- src/indexed-access-types.ts
+++ src/indexed-access-types.ts
@@ -3,6 +3,7 @@
 
 export const nothing = null;
 
+
 // インデックス型？
 // []？
 // 指定した型のプロパティの型を取得できる
@@ -14,11 +15,31 @@ type Person = {
 type Age = Person["age"];
 
 
-// keyof演算子を利用できる
+// []の中にkeyof演算子を利用できる
 // type T = string | number
 type T1 = Person[keyof Person];
 
-// 別の型を指定できる
+
+// []の中に別の型を指定できる
 type T3 = "name" | "age";
 type T4 = Person[T3];
 
+
+// 存在しない型はコンパイルエラー
+// Property 'x' does not exist on type 'Person'.
+// type T5 = Person["x"];
+
+
+// typeo演算子と配列[number]を使うと
+// 配列リテラルの要素の型を取得できる
+const MyArray = [
+	{ name: "Alice", age: 20 },
+	{ name: "Bob", age: 21 }
+];
+// type Person2 = {
+//   name: string;
+//   age: number;
+// }
+type Person2 = typeof MyArray[number];
+// type Age2 = number
+type Age2 = typeof MyArray[number]["age"];
