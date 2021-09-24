065fe4829fa082a9019ab0170f18e05d1ef2cb47
ichiroku11 <toolbox0x10@gmail.com>
2021-09-24 11:15:01 +0900

    Add Conditional Types sample

--- src/handbook/types-from-types/conditional-types.ts
+++ src/handbook/types-from-types/conditional-types.ts
@@ -6,3 +6,30 @@ export const nothing = null;
 
 // SomeType extends OtherType ? TrueType : FalseType;
 
+
+interface IdLabel {
+	id: number;
+}
+
+interface NameLabel {
+	name: string;
+}
+
+// 型引数がnumberの場合はIdLabel、stringの場合はNameLabel
+type IdOrNameLabel<TValue extends number | string>
+	= TValue extends number
+		? IdLabel
+		: NameLabel;
+
+// 関数のオーバーロードが不要になる
+function createLabel<TValue extends number | string>(value: TValue): IdOrNameLabel<TValue> {
+	console.log(typeof value);
+	// todo: 実装方法がわからない
+	throw "Unimplemented";
+}
+
+// _id: IdLabel
+const _id = createLabel(1);
+
+// _name: NameLabel
+const _name = createLabel("x");
