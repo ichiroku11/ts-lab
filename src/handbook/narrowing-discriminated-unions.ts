a64b120d62b483ae9ae5b5750e8fa70da85c9c70
ichiroku11 <toolbox0x10@gmail.com>
2022-01-22 10:09:52 +0900

    Add narrowing-discriminated-unions.ts

---
+++ src/handbook/narrowing-discriminated-unions.ts
@@ -0,0 +1,40 @@
+// https://www.typescriptlang.org/docs/handbook/2/narrowing.html#discriminated-unions
+// https://typescript-jp.gitbook.io/deep-dive/type-system/discriminated-unions
+
+export const nothing = null;
+
+// Discriminated union
+// That got rid of the error! When every type in a union contains a common property with literal types,
+// TypeScript considers that to be a discriminated union, and can narrow out the members of the union.
+
+// すべてのユニオン型が共通のリテラル型のプロパティを持つとき、
+// ユニオン型のメンバーを絞り込める
+
+interface Circle {
+	kind: "circle";
+	radius: number;
+}
+
+interface Square {
+	kind: "square";
+	sideLength: number;
+}
+
+type Shape = Circle | Square;
+
+// Shapeの面積を求める
+function getArea(shape: Shape): number {
+	if (shape.kind === "circle") {
+		// Circle
+		return Math.PI * shape.radius ** 2;
+	} else {
+		// Square
+		return shape.sideLength ** 2;
+	}
+}
+
+// 314.***
+console.log(getArea({ kind: "circle", radius: 10 }));
+
+// 400
+console.log(getArea({ kind: "square", sideLength: 20 }));
