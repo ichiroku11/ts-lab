629d5e0f91c073db4b0ba640157177bbb2dbf116
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2022-09-20 07:52:44 +0900

    Use TestContext.step

---
+++ src/deno/steps.test.ts
@@ -0,0 +1,26 @@
+
+import {
+	assert,
+} from "testing/asserts.ts";
+
+Deno.test("step_ステップを確認する", async (context) => {
+	await context.step("ステップ1", () => {
+		assert(true);
+	});
+
+	await context.step("ステップ2", () => {
+		assert(true);
+	});
+
+	await context.step("ステップ3", () => {
+		assert(true);
+	});
+});
+
+Deno.test("step_ステップをforofで確認する", async (context) => {
+	for (const step of [1, 2, 3]) {
+		await context.step(`ステップ${step}`, () => {
+			assert(true);
+		});
+	}
+});
