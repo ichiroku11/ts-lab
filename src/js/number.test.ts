9431c34d4fdbdac7f452ca0d0d74f73818995b40
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2022-09-23 08:58:24 +0900

    Test Number.toString

---
+++ src/js/number.test.ts
@@ -0,0 +1,26 @@
+import {
+	assertEquals
+} from "testing/asserts.ts";
+
+
+// Number.toString
+// https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Number/toString
+Deno.test("toString_2進数表記の文字列を取得する", async (context) => {
+	// Arrange
+	const testData = [
+		[0, "0"],
+		[1, "1"],
+		[255, "11111111"]
+	];
+
+	for (const [source, expected] of testData) {
+		await context.step(`${source}.toString(2) => ${expected}`, () => {
+			// Act
+			const actual = source.toString(2);
+			// Assert
+			assertEquals(actual, expected);
+		});
+	}
+});
+
+
