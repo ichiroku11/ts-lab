b2caa96270181b1f051dd56fd45b943f9ff9fe80
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2022-09-23 08:45:20 +0900

    Use TestContext.step

--- src/ipv4.test.ts
+++ src/ipv4.test.ts
@@ -5,8 +5,14 @@ import {
 } from "testing/asserts.ts";
 
 
-Deno.test("toBinary_10進数表現を2進数表現に変換できる", () => {
-	const actual = IPv4.toBinary("");
-	const expected = "";
-	assertEquals(actual, expected);
+Deno.test("toBinary_10進数表現を2進数表現に変換できる", async (context) => {
+	const testData = [
+		["", ""]
+	];
+	for (const [ source, expected ] of testData) {
+		await context.step(`${source} => ${expected}`, () => {
+			const actual = IPv4.toBinary(source);
+			assertEquals(actual, expected);
+		});
+	}
 });
