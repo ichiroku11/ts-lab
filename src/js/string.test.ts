b90044eeffae61a79f7d6d6ab5a3448bdced8bd0
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2022-09-23 11:22:16 +0900

    Test String,padStart

---
+++ src/js/string.test.ts
@@ -0,0 +1,23 @@
+import {
+	assertEquals
+} from "testing/asserts.ts";
+
+// String.padStart
+// https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
+Deno.test("padStart_文字列の左側から0埋めできる", async (context) => {
+	// Arrange
+	const testData = [
+		["1", "00000001"],
+		["11111111", "11111111"]
+	];
+
+	for (const [source, expected] of testData) {
+		await context.step(`${source}.pardStart(8, "0") => ${expected}`, () => {
+			// Act
+			const actual = source.padStart(8, "0");
+
+			// Assert
+			assertEquals(actual, expected);
+		});
+	}
+});
