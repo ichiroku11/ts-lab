b24fc88f2fbb6395a2a2513fb5592276e2105383
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2022-09-28 08:12:46 +0900

    Fix comment

--- src/js/regexp.test.ts
+++ src/js/regexp.test.ts
@@ -4,7 +4,7 @@ import {
 
 // RegExp.test
 // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/RegExp/test
-Deno.test("RegExp.test_正規表現を確認する", async (context) => {
+Deno.test(`RegExp.test_数字と"."と"/"にマッチする文字列を確認する`, async (context) => {
 	const testData: [string, boolean][] = [
 		// [source, expected]
 		["0", true],
@@ -15,7 +15,7 @@ Deno.test("RegExp.test_正規表現を確認する", async (context) => {
 	];
 
 	// Arrange
-	const regexp = /[0-9./]/;
+	const regexp = /[0-9\.\/]/;
 
 	for (const [source, expected] of testData) {
 		await context.step(`${regexp}.test("${source}") => ${expected}`, () => {
