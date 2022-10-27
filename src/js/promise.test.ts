a32d582d48f82cd9500f74a9682c3987f0f3f721
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2022-10-28 08:29:53 +0900

    Test Promise.all

--- src/js/promise.test.ts
+++ src/js/promise.test.ts
@@ -1,7 +1,25 @@
 import {
+	assert,
 	assertEquals,
 	assertRejects
 } from "testing/asserts.ts";
+
+// 参考
+// https://azu.github.io/promises-book/
+// https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Promise
+// https://developer.mozilla.org/ja/docs/Web/JavaScript/Guide/Using_promises
+
+Deno.test("Promise.constructor_インスタンスを生成してみる", async () => {
+	// Arrange
+	// Act
+	const promise = new Promise((resolve, _) => {
+		resolve(1);
+	});
+
+	// Assert
+	assert(promise instanceof Promise);
+	assertEquals(1, await promise);
+});
 
 // Promise.resolve
 // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Promise/resolve
@@ -29,4 +47,15 @@ Deno.test("Promise.reject_動きを確認する", async () => {
 		Error,
 		"error!"
 	);
+});
+
+// Promise.all
+// https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Promise/all
+Deno.test("Promise.all_動きを確認する", async () => {
+	// Arrange
+	// Act
+	const actual = await Promise.all([Promise.resolve(1), "x", Promise.resolve(true)]);
+
+	// Assert
+	assertEquals(actual, [1, "x", true]);
 });
