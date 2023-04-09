adfa08dcd56d865aa299aab4510f108d6aef72ac
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2023-04-09 09:50:45 +0900

    Test Boolean

--- src/js/boolean.test.ts
+++ src/js/boolean.test.ts
@@ -1,11 +1,12 @@
 import {
-	assertEquals
+	assertEquals,
+	assertFalse
 } from "testing/asserts.ts";
 
 // Boolean
 // コンストラクターを関数として呼び出してBooleanに変換する
-Deno.test("Boolean_booleanに変換する", async (context) => {
-	const testData: [number | bigint | string | null | undefined | Record<string, unknown>, boolean][] = [
+Deno.test("Boolean_numberとbigintをbooleanに変換する", async (context) => {
+	const testData: [number | bigint, boolean][] = [
 		// [source, expected]
 		// number
 		// 0とNaN以外はtrue
@@ -19,12 +20,6 @@ Deno.test("Boolean_booleanに変換する", async (context) => {
 		[1n, true],
 		[-1n, true],
 		[0n, false],
-
-		// todo:
-		// string
-		// null
-		// undefined
-		// object
 	];
 	for (const [source, expected] of testData) {
 		await context.step(`Boolean(${source}) => ${expected}`, () => {
@@ -37,3 +32,46 @@ Deno.test("Boolean_booleanに変換する", async (context) => {
 		});
 	}
 });
+
+Deno.test("Boolean_stringをbooleanに変換する", async (context) => {
+	const testData: [string, boolean][] = [
+		// [source, expected]
+		// string
+		// 空文字（""）以外はtrue
+		["", false],
+		["a", true]
+	];
+	for (const [source, expected] of testData) {
+		await context.step(`Boolean("${source}") => ${expected}`, () => {
+			// Arrange
+			// Act
+			const actual = Boolean(source);
+
+			// Assert
+			assertEquals(actual, expected);
+		});
+	}
+});
+
+Deno.test("Boolean_nullをbooleanに変換するとfalse", () => {
+	// Arrange
+	// Act
+	const result = Boolean(null);
+
+	// Assert
+	assertFalse(result);
+});
+
+Deno.test("Boolean_undefinedをbooleanに変換するとfalse", () => {
+	// Arrange
+	// Act
+	const result = Boolean(undefined);
+
+	// Assert
+	assertFalse(result);
+});
+
+/*
+// todo:
+// object
+*/
