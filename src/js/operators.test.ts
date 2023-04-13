101588dc68b2e64080e920f279e74dbec37694dc
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2023-04-14 06:39:53 +0900

    Refactor

--- src/js/operators.test.ts
+++ src/js/operators.test.ts
@@ -18,40 +18,46 @@ Deno.test("DestructuringAssignment_残余要素に代入する要素がない場
 // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Operators/Logical_AND
 // x && y
 // xを真偽値に変換した結果がfalseならばxを返し、trueならばyを返す
-Deno.test("LogicalAnd_真偽値以外も扱える1", () => {
-	// Arrange
-	// Act
-	const actual = 1 && 0;
+Deno.test("LogicalAnd_真偽値以外も扱える", async (context) => {
+	await context.step("左オペランドがtrue => 右オペランドの値", () => {
+		// Arrange
+		// Act
+		const actual = 1 && 0;
 
-	// Assert
-	assertEquals(actual, 0);
-});
-Deno.test("LogicalAnd_真偽値以外も扱える2", () => {
-	// Arrange
-	// Act
-	const actual = "" && "a";
+		// Assert
+		assertEquals(actual, 0);
+	});
 
-	// Assert
-	assertEquals(actual, "");
+	await context.step("左オペランドがfalse => 左オペランドの値", () => {
+		// Arrange
+		// Act
+		const actual = "" && "a";
+
+		// Assert
+		assertEquals(actual, "");
+	});
 });
 
 // 論理和
 // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Operators/Logical_OR
 // x || y
 // xを真偽値に変換した結果がtrueならばxを返し、falseならばyを返す
-Deno.test("LogicalOr_真偽値以外も扱える1", () => {
-	// Arrange
-	// Act
-	const actual = 1 || 0;
+Deno.test("LogicalOr_真偽値以外も扱える", async (context) => {
+	await context.step("左オペランドがtrue => 左オペランドの値", () => {
+		// Arrange
+		// Act
+		const actual = 1 || 0;
 
-	// Assert
-	assertEquals(actual, 1);
-});
-Deno.test("LogicalOr_真偽値以外も扱える2", () => {
-	// Arrange
-	// Act
-	const actual = "" || "a";
+		// Assert
+		assertEquals(actual, 1);
+	});
 
-	// Assert
-	assertEquals(actual, "a");
+	await context.step("左オペランドがfalse => 右オペランドの値", () => {
+		// Arrange
+		// Act
+		const actual = "" || "a";
+
+		// Assert
+		assertEquals(actual, "a");
+	});
 });
