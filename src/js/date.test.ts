2f031aea2e207cd4e1512a4c7eb50ad6e71e550e
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2023-05-31 06:02:34 +0900

    Test Date.getTime

--- src/js/date.test.ts
+++ src/js/date.test.ts
@@ -2,6 +2,17 @@ import {
 	assertEquals
 } from "testing/asserts.ts";
 
+Deno.test("getTime_UNIX時間を取得できる", () => {
+	// Arrange
+	const date = new Date("1970-01-01T00:00:00.000Z");
+
+	// Act
+	const actual = date.getTime();
+
+	// Assert
+	assertEquals(actual, 0);
+});
+
 Deno.test("toISOString_ISO 8601形式の文字列を取得できる", () => {
 	// Arrange
 	const expected = "2023-05-30T01:02:03.000Z"
