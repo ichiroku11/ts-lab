b15fb4ffa7348e575db05d39b6ff37fe9aad4eee
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2024-04-19 06:04:52 +0900

    Test indexed access types

--- src/typescriptbook/indexed-access-type.test.ts
+++ src/typescriptbook/indexed-access-type.test.ts
@@ -11,16 +11,24 @@ type User = {
 	name: string,
 	age: number,
 };
-
 // type Type1 = string
 type Type1 = User["name"];
-
 // type Type2 = string | number
 type Type2 = User["name" | "age"];
-
 // keyof演算子とオブジェクトのすべてのプロパティ型がユニオン型で得られる
 // type TYpe3 = string | number
 type Type3 = User[keyof User];
+
+
+// 配列の場合
+type StringValues = string[];
+// type Type4 = string
+type Type4 = StringValues[number];
+
+// タプルの場合
+type Tuple = [string, number];
+// type Type5 = string | number
+type Type5 = Tuple[0 | 1];
 
 // あまり意味のないテスト
 Deno.test("indexed-access-type_動きを確認する", () => {
@@ -29,9 +37,13 @@ Deno.test("indexed-access-type_動きを確認する", () => {
 	const v1: Type1 = "abc";
 	const v2: Type2 = "abc";
 	const v3: Type3 = "abc";
+	const v4: Type4 = "abc";
+	const v5: Type5 = "abc";
 
 	// Assert
 	assertStrictEquals(v1, "abc");
 	assertStrictEquals(v2, "abc");
 	assertStrictEquals(v3, "abc");
+	assertStrictEquals(v4, "abc");
+	assertStrictEquals(v5, "abc");
 });
