1a49280c1d94de5dee4fd904a1ecd39d99fb7074
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2024-04-20 13:38:57 +0900

    Test keyof operator

--- src/typescriptbook/keyof-type-operator.ts
+++ src/typescriptbook/keyof-type-operator.ts
@@ -25,3 +25,43 @@ Deno.test("keyof-type-operator_動きを確認する", () => {
 	assertStrictEquals(key1, "name");
 	assertStrictEquals(key2, "price");
 });
+
+// 書籍「TypeScript入門」p.281より
+// typeofとkeyofを組み合わせて、変数からオブジェクトのプロパティ名を取得する
+const mmConversions = {
+	mm: 1,
+	m: 1000,
+	km: 1000 * 1000,
+};
+// type Unit = "mm" | "m" | "km"
+type Unit = keyof typeof mmConversions;
+function convertUnit(value: number, from: Unit, to: Unit): number {
+	const valueMm = value * mmConversions[from];
+	return valueMm / mmConversions[to];
+}
+
+Deno.test("keyof-type-operator_convertUnit関数の動きを確認する", async (context) => {
+	const testData: [number, Unit, Unit, number][] = [
+		// number, Unit, Unit, number
+		[1, "mm", "mm", 1],
+		[1, "m", "mm", 1000],
+		[1, "km", "mm", 1000 * 1000],
+		[10, "mm", "mm", 10],
+		[10, "m", "mm", 1000 * 10],
+		[10, "km", "mm", 1000 * 1000 * 10],
+		[1, "mm", "m", 1 / 1000],
+		[1, "mm", "km", 1 / (1000 * 1000)],
+
+	];
+
+	for (const [source, from, to, expected] of testData) {
+		await context.step(`convertUnit(${source}, "${from}", "${to}")} => ${expected}`, () => {
+			// Arrange
+			// Act
+			const actual = convertUnit(source, from, to);
+
+			// Assert
+			assertStrictEquals(actual, expected);
+		});
+	}
+});
