d1564cf0fbe9122923b483bb5e4b4a9941da8b86
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2024-04-22 06:04:43 +0900

    Refactor

--- src/js/promise.test.ts
+++ src/js/promise.test.ts
@@ -1,15 +1,17 @@
 import {
 	assert,
 	assertEquals,
-	assertRejects
+	assertRejects,
+	assertStrictEquals
 } from "testing/asserts.ts";
 
 // 参考
 // https://azu.github.io/promises-book/
+// https://typescriptbook.jp/reference/asynchronous/promise
 // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Promise
 // https://developer.mozilla.org/ja/docs/Web/JavaScript/Guide/Using_promises
 
-Deno.test("Promise.constructor_インスタンスを生成してみる", async () => {
+Deno.test("Promise.constructor_コンストラクターで処理が成功したインスタンスを生成する", async () => {
 	// Arrange
 	// Act
 	const promise = new Promise((resolve, _) => {
@@ -18,12 +20,12 @@ Deno.test("Promise.constructor_インスタンスを生成してみる", async (
 
 	// Assert
 	assert(promise instanceof Promise);
-	assertEquals(1, await promise);
+	assertStrictEquals(1, await promise);
 });
 
 // Promise.resolve
 // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Promise/resolve
-Deno.test("Promise.resolve_動きを確認する", async () => {
+Deno.test("Promise.resolve_処理が成功したインスタンスを生成する", async () => {
 	// Arrange
 	// Act
 	const actual = await Promise.resolve(1);
@@ -31,12 +33,12 @@ Deno.test("Promise.resolve_動きを確認する", async () => {
 	//const actual = new Promise(resolve => resolve(1));
 
 	// Assert
-	assertEquals(1, actual);
+	assertStrictEquals(1, actual);
 });
 
 // Promise.reject
 // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Promise/reject
-Deno.test("Promise.reject_動きを確認する", async () => {
+Deno.test("Promise.reject_処理が失敗したインスタンスを生成する", async () => {
 	// assertRejectsを使うと、Promiseから例外が発生することを確認できる
 	// https://deno.land/manual@v1.26.2/testing/assertions#throws
 	// Arrange
@@ -51,7 +53,7 @@ Deno.test("Promise.reject_動きを確認する", async () => {
 
 // Promise.all
 // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Promise/all
-Deno.test("Promise.all_動きを確認する", async () => {
+Deno.test("Promise.all_すべての処理が成功したときに成功するインスタンスを生成する", async () => {
 	// Arrange
 	// Act
 	const actual = await Promise.all([Promise.resolve(1), "x", Promise.resolve(true)]);
