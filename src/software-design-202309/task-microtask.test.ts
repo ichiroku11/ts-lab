03e5df906dc34fb245c7b5db0d66007aac3b83b5
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2024-04-25 06:02:28 +0900

    Test task and microtask

---
+++ src/software-design-202309/task-microtask.test.ts
@@ -0,0 +1,34 @@
+import {
+	assertEquals,
+} from "testing/asserts.ts";
+
+// p.39
+Deno.test("タスクとマイクロタスクの実行順を確認する", async () => {
+	// Arrange
+	const steps: number[] = [];
+
+	// Act
+	steps.push(1);
+
+	// setTimeoutはタスク
+	setTimeout(() => {
+		// このtestの中では、このコールバックは呼ばれない
+		steps.push(5);
+	});
+
+	// Promiseはマイクロタスク
+	const promise = Promise.resolve()
+		.then(() => {
+			steps.push(3);
+		})
+		.then(() => {
+			steps.push(4);
+		});
+
+	steps.push(2);
+
+	await promise;
+
+	// Assert
+	assertEquals(steps, [1, 2, 3, 4]);
+});
