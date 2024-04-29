c4a6c6254f5fe8cee25a561c2aee1a3cbc90cf08
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2024-04-30 06:04:27 +0900

    Test AbortSignal.throwIfAborted

--- src/web-api/abort-signal.test.ts
+++ src/web-api/abort-signal.test.ts
@@ -1,6 +1,7 @@
 import {
 	assert,
 	assertStrictEquals,
+	assertThrows,
 } from "testing/asserts.ts";
 
 // https://developer.mozilla.org/ja/docs/Web/API/AbortSignal
@@ -14,4 +15,21 @@ Deno.test("AbortSignal.abort_abortされたAbortSignalを生成する", () => {
 	// Assert
 	assert(signal.aborted);
 	assertStrictEquals(signal.reason, "abort!");
+});
+
+// AbortSignal.throwIfAborted
+Deno.test("AbortSignal.throwIfAborted_abortされたAbortSignalからは例外が発生する", () => {
+	// Arrange
+	const signal = AbortSignal.abort("abort!");
+
+	// Act
+	// Assert
+	const error = assertThrows(
+		() => signal.throwIfAborted(),
+		"abort!");
+
+	// Errorインスタンスではなくstring
+	// abortに渡した型か？
+	assertStrictEquals(typeof error, "string");
+	assertStrictEquals(error, "abort!");
 });
