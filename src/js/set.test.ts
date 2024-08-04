278851b656cdfb75364e990d2fb4d0e2d9c66b47
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2024-08-04 19:36:26 +0900

    Test Set.intersection

--- src/js/set.test.ts
+++ src/js/set.test.ts
@@ -14,3 +14,15 @@ Deno.test("intersection_両方の要素を含むSetを返す", () => {
 	// Assert
 	assertEquals([...actual], [2, 3]);
 });
+
+Deno.test("intersection_空のSetを指定すると空のSetを返す", () => {
+	// Arrange
+	const set1 = new Set([1, 2, 3]);
+	const set2 = new Set();
+
+	// Act
+	const actual = set1.intersection(set2);
+
+	// Assert
+	assertEquals([...actual], []);
+});
