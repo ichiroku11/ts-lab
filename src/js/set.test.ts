06b769b890d287a9668205a26cddad2afff90865
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2024-08-04 19:43:07 +0900

    Refactor

--- src/js/set.test.ts
+++ src/js/set.test.ts
@@ -3,26 +3,32 @@ import {
 } from "testing/asserts.ts";
 
 // https://web.dev/blog/set-methods
-Deno.test("intersection_両方の要素を含むSetを返す", () => {
-	// Arrange
-	const set1 = new Set([1, 2, 3]);
-	const set2 = new Set([2, 3, 4]);
+Deno.test("intersection", async (context) => {
+	await context.step("両方の要素を含むSetを返す", () => {
+		// Arrange
+		const set1 = new Set([1, 2, 3]);
+		const set2 = new Set([2, 3, 4]);
 
-	// Act
-	const actual = set1.intersection(set2);
+		// Act
+		const actual1 = set1.intersection(set2);
+		const actual2 = set2.intersection(set1);
 
-	// Assert
-	assertEquals([...actual], [2, 3]);
-});
+		// Assert
+		assertEquals([...actual1], [2, 3]);
+		assertEquals([...actual2], [2, 3]);
+	});
 
-Deno.test("intersection_空のSetを指定すると空のSetを返す", () => {
-	// Arrange
-	const set1 = new Set([1, 2, 3]);
-	const set2 = new Set();
-
-	// Act
-	const actual = set1.intersection(set2);
-
-	// Assert
-	assertEquals([...actual], []);
+	await context.step("空のSetを指定すると空のSetを返す", () => {
+		// Arrange
+		const set1 = new Set([1, 2, 3]);
+		const set2 = new Set();
+	
+		// Act
+		const actual1 = set1.intersection(set2);
+		const actual2 = set2.intersection(set1);
+	
+		// Assert
+		assertEquals([...actual1], []);
+		assertEquals([...actual2], []);
+	});
 });
