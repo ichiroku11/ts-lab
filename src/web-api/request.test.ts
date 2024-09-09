325ce44d74f0c9df0e814455d340f48090d65358
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2024-09-10 05:46:45 +0900

    Test Request

--- src/web-api/request.test.ts
+++ src/web-api/request.test.ts
@@ -1,18 +1,33 @@
 import {
 	assertEquals,
-	assertExists
+	assertExists,
+	assertThrows
 } from "testing/asserts.ts";
 
-Deno.test("Request_インスタンスを確認する", () => {
-	// Arrange
-	// Act
-	const acutal = new Request("https://localhost");
+Deno.test("Request", async (context) => {
+	await context.step("生成したインスタンスを確認する", () => {
+		// Arrange
+		// Act
+		const acutal = new Request("https://localhost");
 
-	// Assert
-	// ヘッダー
-	assertExists(acutal.headers)
-	assertEquals(acutal.headers.keys.length, 0)
+		// Assert
+		// ヘッダー
+		assertExists(acutal.headers)
+		assertEquals(acutal.headers.keys.length, 0)
 
-	// ボディ
-	assertEquals(acutal.body, null)
+		// ボディ
+		assertEquals(acutal.body, null)
+	});
+
+	await context.step("引数が空文字だと例外が発生する", () => {
+		// Arrange
+		// Act
+		// Assert
+		assertThrows(
+			() => {
+				new Request("");
+			},
+			TypeError,
+			"Invalid URL: ''");
+	});
 });
