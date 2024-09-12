08f1d32520cb6d72ea69ad06c9641abc691ffc96
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2024-09-13 05:50:33 +0900

    Test Response.redirect

--- src/web-api/response.test.ts
+++ src/web-api/response.test.ts
@@ -19,3 +19,20 @@ Deno.test("Response.error_生成されるインスタンスを確認する", () 
 	// ボディ
 	assertEquals(acutal.body, null)
 });
+
+Deno.test("Response.redirect_生成されるインスタンスを確認する", () => {
+	// Arrange
+	// Act
+	const acutal = Response.redirect("https://localhost");
+
+	// Assert
+	// ステータスコード
+	assertEquals(acutal.status, 302);
+
+	// ヘッダー
+	assertExists(acutal.headers)
+	assertEquals(acutal.headers.keys.length, 0)
+
+	// ボディ
+	assertEquals(acutal.body, null)
+});
