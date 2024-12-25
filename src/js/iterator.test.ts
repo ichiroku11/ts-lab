680dff15fbb8eb76a9d8a31c4001f8c453ff6fca
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2024-12-26 05:57:02 +0900

    Test iterator

---
+++ src/js/iterator.test.ts
@@ -0,0 +1,45 @@
+import {
+	assertEquals
+} from "testing/asserts.ts";
+
+// 参考
+// https://techblog.gmo-ap.jp/2024/11/19/js-generator-yield/
+// https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Iterator
+
+// イテレーターは、イテレータープロトコルに適合するオブジェクト
+// 最小限nextメソッドを持つ
+Deno.test("next", () => {
+	// Arrange
+	let index = 0;
+	const iterator: Iterator<number> = {
+		next(): IteratorResult<number> {
+			index++;
+
+			if(index > 3) {
+				return {
+					done: true,
+					value: undefined
+				}
+			}
+
+			return {
+				done: false,
+				value: index
+			};
+		}
+	};
+
+	// Act
+	const actual = [
+		iterator.next(),
+		iterator.next(),
+		iterator.next(),
+		iterator.next(),
+	];
+
+	// Assert
+	assertEquals(actual[0], { done: false, value: 1 });
+	assertEquals(actual[1], { done: false, value: 2 });
+	assertEquals(actual[2], { done: false, value: 3 });
+	assertEquals(actual[3], { done: true, value: undefined });
+});
