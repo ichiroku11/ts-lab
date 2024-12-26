94274fe410a2440eddfb928e2296e74aa4b5deee
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2024-12-27 05:47:28 +0900

    Test iterator

--- src/js/iterator.test.ts
+++ src/js/iterator.test.ts
@@ -8,18 +8,19 @@ import {
 
 // イテレーターは、イテレータープロトコルに適合するオブジェクト
 // 最小限nextメソッドを持つ
-Deno.test("next", () => {
+// nextメソッドは、IteratorResultオブジェクトを返す
+Deno.test("値を生成するイテレーターのサンプル", () => {
 	// Arrange
 	let index = 0;
 	const iterator: Iterator<number> = {
 		next(): IteratorResult<number> {
 			index++;
 
-			if(index > 3) {
+			if (index > 3) {
 				return {
 					done: true,
 					value: undefined
-				}
+				};
 			}
 
 			return {
@@ -28,6 +29,27 @@ Deno.test("next", () => {
 			};
 		}
 	};
+
+	// Act
+	const actual = [
+		iterator.next(),
+		iterator.next(),
+		iterator.next(),
+		iterator.next(),
+	];
+
+	// Assert
+	assertEquals(actual[0], { done: false, value: 1 });
+	assertEquals(actual[1], { done: false, value: 2 });
+	assertEquals(actual[2], { done: false, value: 3 });
+	assertEquals(actual[3], { done: true, value: undefined });
+});
+
+Deno.test("値を取り出すイテレーターのサンプル", () => {
+	// Arrange
+	const array = [1, 2, 3];
+	// ArrayIterator
+	const iterator = array[Symbol.iterator]();
 
 	// Act
 	const actual = [
