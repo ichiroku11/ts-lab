eb77ef52cfd37908a0cd9b2221d486e4d2e772d1
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2024-12-28 11:22:13 +0900

    Test iterable

---
+++ src/js/iterable.test.ts
@@ -0,0 +1,49 @@
+import {
+	assertEquals
+} from "testing/asserts.ts";
+
+// 参考
+// https://techblog.gmo-ap.jp/2024/11/19/js-generator-yield/
+// https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Iteration_protocols
+
+// イテラブル = 反復可能プロコトル
+// for...ofで列挙できる
+Deno.test("イテラブルのサンプル", () => {
+	// Arrange
+	let index = 0;
+	const iterator: Iterator<number> = {
+		next(): IteratorResult<number> {
+			index++;
+
+			if (index > 3) {
+				return {
+					done: true,
+					value: undefined
+				};
+			}
+
+			return {
+				done: false,
+				value: index
+			};
+		}
+	};
+
+	const iterable: Iterable<number> = {
+		[Symbol.iterator]: () => {
+			return iterator
+		}
+	};
+
+	const actual: number[] = [];
+
+	// Act
+	for (const value of iterable) {
+		actual.push(value);
+	}
+
+	// Assert
+	assertEquals(actual[0], 1);
+	assertEquals(actual[1], 2);
+	assertEquals(actual[2], 3);
+});
