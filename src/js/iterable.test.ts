db0ee2553671e3ecd4cdd8536195914f56c43a1b
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2024-12-29 10:08:00 +0900

    Test IterableIterator

--- src/js/iterable.test.ts
+++ src/js/iterable.test.ts
@@ -8,7 +8,7 @@ import {
 
 // イテラブル = 反復可能プロコトル
 // for...ofで列挙できる
-Deno.test("イテラブルのサンプル", () => {
+Deno.test("Iterable_サンプル", () => {
 	// Arrange
 	let index = 0;
 	const iterator: Iterator<number> = {
@@ -31,7 +31,7 @@ Deno.test("イテラブルのサンプル", () => {
 
 	const iterable: Iterable<number> = {
 		[Symbol.iterator]: () => {
-			return iterator
+			return iterator;
 		}
 	};
 
@@ -46,4 +46,37 @@ Deno.test("イテラブルのサンプル", () => {
 	assertEquals(actual[0], 1);
 	assertEquals(actual[1], 2);
 	assertEquals(actual[2], 3);
+});
+
+Deno.test("IterableIterator_サンプル", () => {
+	// Arrange
+	let index = 0;
+	const iterator: IterableIterator<number> = {
+		next(): IteratorResult<number> {
+			index++;
+
+			if (index > 3) {
+				return {
+					done: true,
+					value: undefined
+				};
+			}
+
+			return {
+				done: false,
+				value: index
+			};
+		},
+		[Symbol.iterator](): IterableIterator<number> {
+			return this;
+		}
+	};
+
+	// Act
+	const actual: number[] = [];
+
+	// Assert
+	for (const value of iterator) {
+		actual.push(value);
+	}
 });
