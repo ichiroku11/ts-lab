3f9b3e55af14f4a1f60eba4f43d82d87e1b81512
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2025-01-27 05:55:45 +0900

    Test Symbol

--- src/js/symbol.test.ts
+++ src/js/symbol.test.ts
@@ -1,5 +1,6 @@
 import {
-	assertEquals
+	assertEquals,
+	assertFalse
 } from "testing/asserts.ts";
 
 // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Symbol
@@ -14,4 +15,22 @@ Deno.test("Symbol_動きを確認する", async (context) => {
 		// Assert
 		assertEquals(actual, "symbol");
 	});
+
+	await context.step("シンボルは一意", () => {
+		// Arrange
+		// Act
+		const actual = Symbol() === Symbol();
+
+		// Assert
+		assertFalse(actual);
+	});
+
+	await context.step("シンボルは一意その2", () => {
+		// Arrange
+		// Act
+		const actual = Symbol("x") === Symbol("x");
+
+		// Assert
+		assertFalse(actual);
+	});	
 });
