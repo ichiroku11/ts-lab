01d32837142358fbcf1b6ae40a0f644acba7c786
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2025-01-30 05:54:32 +0900

    Test Object.getOwnPropertySymbols

--- src/js/object.test.ts
+++ src/js/object.test.ts
@@ -17,3 +17,35 @@ Deno.test("Object.getOwnPropertyNames", async (context) => {
 		assertEquals(actual, ["a", "b", "c"]);
 	});
 });
+
+Deno.test("Object.getOwnPropertySymbols", async (context) => {
+	await context.step("シンボルプロパティを配列として取得する", () => {
+		// Arrange
+		const a = Symbol.for("a");
+		const b = Symbol.for("b");
+
+		const obj = {
+			[a]: 1,
+			[b]: 2,
+		};
+
+		// Act
+		const actual = Object.getOwnPropertySymbols(obj);
+
+		// Assert
+		// todo: 並び順？
+		assertEquals(actual, [a, b]);
+	});
+
+	await context.step("シンボルプロパティが存在しないオブジェクトを引数に指定すると戻り値は空配列", () => {
+		// Arrange
+		const obj = {
+		};
+
+		// Act
+		const actual = Object.getOwnPropertySymbols(obj);
+
+		// Assert
+		assertEquals(actual.length, 0);
+	});
+});
