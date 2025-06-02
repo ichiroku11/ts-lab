df0d7e498a192e5755e22a4d14861162fd8e66a3
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2025-06-03 05:45:14 +0900

    Iterator.forEach

--- src/js/iterator.test.ts
+++ src/js/iterator.test.ts
@@ -136,6 +136,30 @@ Deno.test("Iterator.some", async (context) => {
 	});
 });
 
+Deno.test("Iterator.forEach", async (context) => {
+	function* generator() {
+		yield "a";
+		yield "b";
+		yield "c";
+	}
+
+	await context.step("それぞれ要素に対して1回ずつコールバック関数が呼び出される", () => {
+		// Arrange
+		const actual:[string, number][] = [];
+
+		// Act
+		generator()
+			.forEach((value, index) => {
+				actual.push([value, index]);
+			});
+
+		// Assert
+		// インデックスは0から始まる
+		assertEquals(actual, [["a", 0], ["b", 1], ["c", 2]]);
+	});
+});
+
+
 Deno.test("Iterator.take", async (context) => {
 	function* generator() {
 		yield 1;
